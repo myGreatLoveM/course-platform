23972f008208e14fc1b616adf24abd73e6e9a2f0
prem <praulayar994@gmail.com>
2024-09-22 10:24:46 +0530

    baseine course model create

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email_required", "Email Required"
+
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "pub", "Published"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "Draft"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    # image
+    access = models.CharField(
+        max_length=10,
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.ANYONE
+    )
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices, 
+        default=PublishStatus.DRAFT)
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
