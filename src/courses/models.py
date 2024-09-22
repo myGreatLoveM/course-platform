b276a9a2736108ad8e34ebda80585ca3be1936a9
prem <praulayar994@gmail.com>
2024-09-22 10:59:33 +0530

    makemigrations and migrate course model

--- src/courses/models.py
+++ src/courses/models.py
@@ -14,18 +14,26 @@ class PublishStatus(models.TextChoices):
     DRAFT = "draft", "Draft"
 
 
+def handle_upload(instance, filename):
+    return f"{filename}"
+
+
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    # image
+    image = models.ImageField(
+        upload_to=handle_upload,
+        blank=True,
+        null=True
+    )
     access = models.CharField(
-        max_length=10,
+        max_length=20,
         choices=AccessRequirement.choices,
         default=AccessRequirement.ANYONE
     )
     status = models.CharField(
-        max_length=10, 
-        choices=PublishStatus.choices, 
+        max_length=20,
+        choices=PublishStatus.choices,
         default=PublishStatus.DRAFT)
 
     @property
